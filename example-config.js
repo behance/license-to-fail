a9ea17b2a4362bef900c3a271d9da5e2acb31450
Henry Zhu <hi@henryzoo.com>
2016-09-01 14:38:41 -0400

    update example-config

--- example-config.js
+++ example-config.js
@@ -1,8 +1,30 @@
 module.exports = {
-  "allowedPackages": [{
-    "name": "spdx-license-ids@1.2.2",
-    "reason": "Unlicense"
-  }],
+  "allowedPackages": [
+    {
+      "name": "spdx-license-ids@1.2.2",
+      "reason": "Unlicense"
+    },
+    {
+      "name": "component-emitter",
+      "repo": "https://github.com/component/emitter",
+      "reason": "MIT but not published"
+    },
+    {
+      "name": "component-inherit",
+      "repo": "https://github.com/component/inherit",
+      "reason": "MIT but not published"
+    },
+    {
+      "name": "debug@0.7.4",
+      "repo": "https://github.com/visionmedia/debug",
+      "reason": "MIT but not published"
+    },
+    {
+      "name": "humane-js@3.2.2",
+      "repo": "https://github.com/wavded/humane-js",
+      "reason": "MIT in the README"
+    }
+  ],
   "disallowedPackages": [],
   "allowedLicenses": [
     "MIT",
