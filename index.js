0111a0c73bb7c608700c8360e83e0015166d58a8
Henry Zhu <hi@henryzoo.com>
2016-11-11 07:29:35 -0500

    check if array, fixes #6

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ module.exports = function checkLicenses(config) {
       console.log('');
       console.log('Disallowed Licenses:');
       prohibitedDeps.sort(function(a, b) {
-        var aLower = a.licenses.toLowerCase();
-        var bLower = b.licenses.toLowerCase();
+        var aLower = Array.isArray(a) ? a.licenses[0].toLowerCase() : a.licenses.toLowerCase();
+        var bLower = Array.isArray(b) ? b.licenses[0].toLowerCase() : b.licenses.toLowerCase();
         return aLower < bLower ? -1 : aLower > bLower ? 1 : 0;
       });
       prohibitedDeps.map(function(dep) { log(dep); });
