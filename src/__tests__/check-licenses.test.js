8a4cdcf6c2e077f5a7439ce0cb67c8dae442497c
Jason Palmer <jason@jason-palmer.com>
2016-12-13 10:32:06 -0500

    Modified test script to execute jest

--- src/__tests__/check-licenses.test.js
+++ src/__tests__/check-licenses.test.js
@@ -1,43 +1,44 @@
+import test from 'ava';
 
-test('should fail on BSD-3-Clause if in strict mode and only BSD is whitelisted', () => {
-  const checkLicenses = require('../check-licenses');
-  const config = {
+test('should fail on BSD-3-Clause if in strict mode and only BSD is whitelisted', (t) => {
+  var checkLicenses = require('../check-licenses');
+  var config = {
     'allowedLicenses': [
       'BSD'
     ],
     'strictMode': true
   };
-  const sampleDependency = {
+  var sampleDependency = {
     name: 'foo@1.0.0',
     licenses: 'BSD-3-Clause',
     repository: 'https://github.com/foo',
     licenseFile: '/path/to/LICENSE'
   };
 
-  expect(checkLicenses(config).isAllowedDependency(sampleDependency)).toBe(false);
+  t.is(checkLicenses(config).isAllowedDependency(sampleDependency), false);
 });
 
-test('should pass if license is BSD or MIT if only BSD is whitelisted in strictMode', () => {
-  const checkLicenses = require('../check-licenses');
-  const config = {
+test('should pass if license is BSD or MIT if only BSD is whitelisted in strictMode', (t) => {
+  var checkLicenses = require('../check-licenses');
+  var config = {
     'allowedLicenses': [
       'BSD'
     ],
     'strictMode': true
   };
-  const sampleDependency = {
+  var sampleDependency = {
     name: 'foo@1.0.0',
     licenses: 'BSD OR MIT',
     repository: 'https://github.com/foo',
     licenseFile: '/path/to/LICENSE'
   };
-  const sampleDependencyWithParens = {
+  var sampleDependencyWithParens = {
     name: 'foo@1.0.0',
     licenses: '(BSD OR MIT)',
     repository: 'https://github.com/foo',
     licenseFile: '/path/to/LICENSE'
   };
 
-  expect(checkLicenses(config).isAllowedDependency(sampleDependency)).toBe(true);
-  expect(checkLicenses(config).isAllowedDependency(sampleDependencyWithParens)).toBe(true);
+  t.is(checkLicenses(config).isAllowedDependency(sampleDependency), true);
+  t.is(checkLicenses(config).isAllowedDependency(sampleDependencyWithParens), true);
 });
